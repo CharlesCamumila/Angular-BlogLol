093886500770666dbb3180c88995e92814d00745
Chares Magalhães <charlesreismagalhaes@outlook.com>
2024-03-14 16:35:31 -0300

    blog do league of legends

---
+++ src/app/components/big-card/big-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-big-card',
+  templateUrl: './big-card.component.html',
+  styleUrls: ['./big-card.component.css']
+})
+export class BigCardComponent {
+
+  @Input()
+  photoCover:string = ""
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription = ""
+  @Input()
+  id:string="0"
+  constructor(){
+
+  }
+
+}
